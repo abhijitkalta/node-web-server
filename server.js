31c0084c2b09686d15d604d09b2b7451de98d363
Abhijit Kalta <abhijitkalta@gmail.com>
2017-07-03 01:41:02 +0530

    Add express server  app

---
+++ server.js
@@ -0,0 +1,66 @@
+"use strict";
+
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var timeStamp = new Date().toString();
+  var log = `Time: ${timeStamp}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log("Unable to log");
+    }
+  });
+  next();
+});
+
+app.use((req, res) =>{
+  res.render('maintenance.hbs',{
+    message: 'Site is under Maintenance!!'
+  })
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+app.get('/', (req, res) => {
+  // res.send("<h1>Hello world!!</h1>");
+  // res.send({
+  //   name: "Abhijit",
+  //   address: "Bangalore",
+  //   age: 25
+  // });
+  res.render("home.hbs", {
+    title: "Home Page",
+    welcomeMessage: 'welcome to my page'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render("about.hbs", {
+    title: "Abouts Page"
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "Unable to handle request"
+  });
+})
+
+app.listen(3000, () => {
+  console.log("Server is up on 3000");
+});
