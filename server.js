b08a6b5a7d0ba02676c5d032eae4aaab788467fa
Abhijit Kalta <abhijitkalta@gmail.com>
2017-07-04 23:55:29 +0530

    Projects page added

--- server.js
+++ server.js
@@ -56,6 +56,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req , res)=> {
+  res.render('projects', {
+    message: "My projects"
+  })
+})
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: "Unable to handle request"
